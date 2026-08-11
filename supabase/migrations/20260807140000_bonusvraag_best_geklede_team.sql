dd8b01519cbcf09b81a81d320fbd74cbb1018765
Freddys-Angels <jaap.litjens@gmail.com>
2026-08-11 21:35:14 +0200

    Bonusvraag toevoegen: best geklede team (donderdag)

---
+++ supabase/migrations/20260807140000_bonusvraag_best_geklede_team.sql
@@ -0,0 +1,7 @@
+-- ============================================================
+-- 20260807140000_bonusvraag_best_geklede_team.sql
+-- Extra bonusvraag Feestweek 2026
+-- ============================================================
+
+insert into bonusvragen (vraag, punten, volgorde) values
+('Welk team wint op donderdag de prijs voor best geklede team?', 15, 8);
