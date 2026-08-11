d20edd865dc91563b2b5b22669ffaabcfffff8f7
Freddys-Angels <jaap.litjens@gmail.com>
2026-08-11 21:38:15 +0200

    Bonusvragen naar 10 punten per vraag

---
+++ supabase/migrations/20260807150000_bonusvragen_10_punten.sql
@@ -0,0 +1,6 @@
+-- ============================================================
+-- 20260807150000_bonusvragen_10_punten.sql
+-- Alle bonusvragen naar 10 punten per vraag
+-- ============================================================
+
+update bonusvragen set punten = 10;
